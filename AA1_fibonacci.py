88c5a2455634c3a619598d4b648fb6d077fb0a48
Tugce Sirin <ztugcesirin@gmail.com>
2015-03-01 23:15:19 +0200

    Rekursif fibonacci hesaplama
    Rekursif olarak verilen sıradaki fibonacci sayısını bulan fonksiyon ve
    zamanlamayı hesaplamak için timer eklendi.

--- AA1_fibonacci.py
+++ AA1_fibonacci.py
@@ -1,3 +1,5 @@
+from time import time
+
 def fibonacci_loop(counter):
     result = 1
     if(counter == 1 or counter == 2):
@@ -13,8 +15,19 @@ def fibonacci_loop(counter):
         i = i + 1
     return result
 
+def fibonacci_recursive(counter):
+    if(counter == 1 or counter == 2):
+        return 1
+    else: return fibonacci_recursive(counter-1)+fibonacci_recursive(counter-2)
+    
 if __name__ == '__main__':
     number = 10
     for i in range(0,7):
+        t0 = time()
         print fibonacci_loop(number)
+        t1 = time()
+        print fibonacci_recursive(number)
+        t2 = time()
         number = number + 10
+        print 'fibonacci_loop zaman = %f' %(t1-t0)
+        print 'fibonacci_recursive zaman = %f' %(t2-t1)
