7f877ef0340f80a79d9c11b200dec36d74b466f6
Tugce Sirin <ztugcesirin@gmail.com>
2015-03-23 11:34:21 +0200

    sol capraz kontrolü eklendi

--- AA_sekizvezirquiz.py
+++ AA_sekizvezirquiz.py
@@ -20,8 +20,41 @@ def create():
                     satranc[j][i] = 0
     for i in range(0,8):
         print satranc[i]
-        
+    return satranc
+def test(tablo):
+    conflictFirst = []
+    conflictSecond = []
+    for i in range(0,8):
+        sutunKontrol = False
+        solCaprazKontrol = False
+        sagCaprazKontrol = False
+        for j in range(0,8):
+            if(tablo[i][j] == 1):
+                if(sutunKontrol == True): continue
+                sutunKontrol = True
+                for m in range(0,8):
+                    if(tablo[i][m] == 1 and m != j):
+                        conf = 'sutun (%d %d) (%d %d) \n' %(i,j,i,m)
+                        conflictFirst.append(conf)
+            if(tablo[i][j] == 1):
+                if(solCaprazKontrol == False):
+                    print "sol capraz kontrol"
+                    solCaprazKontrol = True
+                    soli = i
+                    solj = j
+                    for n in range(0,8):
+                        if(soli != 0 and solj!= 0):
+                            soli = soli -1
+                            solj = solj -1
+                            if(tablo[soli][solj] == 1):
+                                conf = ' sol capraz(%d %d) (%d %d) \n' %(i,j,soli,solj)
+                                conflictFirst.append(conf)
+    for i in range(0, len(conflictFirst)):
+        print conflictFirst[i]
+    
 if __name__ == '__main__':
-    create()
+    t = create()
+    test(t)
+    
     
     
