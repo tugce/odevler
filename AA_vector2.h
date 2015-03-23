5f3a5511ba362c114f5991988b0099c0d494795f
Tugce Sirin <ztugcesirin@gmail.com>
2015-03-23 09:43:52 +0200

    adres konumlarını görmek için pointerlar eklendi

--- AA_vector2.h
+++ AA_vector2.h
@@ -6,14 +6,18 @@ using namespace std;
 int main(int argc, char *argv[]){
 
 	std::vector<int> dynamicArray;
+	int *p1,*p2;
 	cout<<"capacity "<<dynamicArray.capacity()<<endl;
 	cout<<"size "<<dynamicArray.size()<<endl;
+	dynamicArray.push_back(5);
+	p1 = &dynamicArray[0];
 	for(int i = 0; i <100; i++){
 		dynamicArray.push_back(253);
 		cout<<"capacity "<<dynamicArray.capacity();
 		cout<<"     size "<<dynamicArray.size()<<endl;
 
 	}
+	p2 = dynamicArray[0];
 	
 	system("PAUSE");
 }
