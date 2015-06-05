cd098dff5c0bf5b3bcd9362982326dc1bb54e84c
Tugce Sirin <ztugcesirin@gmail.com>
2015-06-05 14:35:15 +0300

    armstrong sayilari için kod

--- AA_armstrong.py
+++ AA_armstrong.py
@@ -1,6 +1,15 @@
+print "tek for lu cozum"
 for i in range(100,999):
     yuzler = i/100
     onlar = (i-yuzler*100)/10
     birler = (i-yuzler*100-onlar*10)
     if(yuzler*yuzler*yuzler + onlar*onlar*onlar + birler*birler*birler == i):
         print "Armastrong sayisi = ",i
+
+#diger yontemle cozum
+print "3 for lu cozum"
+for i in range(1,9):
+    for j in range(0,9):
+        for k in range(0,9):
+            if(i*100+j*10+k == i*i*i + j*j*j + k*k*k):
+                print "Armstrong sayisi = ",i*100+j*10+k
